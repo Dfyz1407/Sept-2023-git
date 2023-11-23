d98716b9fca49f7eac111f7eb4716a5bec15ed74
Dfyz <Dfyz1407@gmail.com>
2023-11-23 16:04:35 +0200

    start project

---
+++ js/1 Lesson/main.js
@@ -0,0 +1,26 @@
+// //console.log("new");
+// let n1 = 123.123;
+// let s1 = 'text';
+// // > <  >= <= == != === !==
+// let b1 = true;
+// let b2 = 5 > 6;
+// let bool1 = 5 == '5'; //  true
+// let bool2 = 5 === '5' // false
+// console.log(bool2);
+//
+// // undefined    null
+
+let a= 10;
+let b = 20;
+
+let c = a+b;
+
+console.log(a+b);
+
+let str1 = 'one';
+let str2 = 'two';
+
+console.log(`${str1} ${str2} !`);
+
+
+console.log(!!100); //true
